1ea3d6c6d0ba720eee9b1821a178f41bfc3b4862
kinderror <dshkinderov@mail.ru>
2022-09-28 17:23:45 +0300

    Copy and paste version from description of the lab

---
+++ labProb.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <random>
+
+using State = int;
+
+class DiscreteState {
+private:
+    State const s0;
+public:
+    DiscreteState(State s0): s0(s0) { }
+    bool contains(State s) const {
+        return s == s0;
+    }
+};
+
+class SegmentState {
+private:
+    State const begin_s0, end_s0;
+public:
+    SegmentState(State begin_s0, State end_s0):
+        begin_s0(begin_s0), end_s0(end_s0) { }
+    bool contains(State s) const {
+        return begin_s0 <= s and end_s0 >= s;
+    }
+};
+
+class ProbabilityTest {
+private:
+    State const E_min, E_max;
+public:
+    ProbabilityTest(State E_min, State E_max): 
+        E_min(E_min), E_max(E_max) { }
+    float test(
+        DiscreteState system,
+        unsigned test_count,
+        unsigned seed) const {
+        std::default_random_engine reng(seed);
+        std::uniform_int_distribution<int> dstr(E_min, E_max);
+        unsigned good = 0;
+        for (unsigned cnt = 0; cnt != test_count; ++cnt)
+            if (system.contains(dstr(reng))) ++good;
+        return
+            static_cast<float>(good)
+            /static_cast<float>(test_count);
+    }
+    float test(
+        SegmentState system,
+        unsigned test_count,
+        unsigned seed) const {
+        std::default_random_engine reng(seed);
+        std::uniform_int_distribution<int> dstr(E_min, E_max);
+        unsigned good = 0;
+        for (unsigned cnt = 0; cnt != test_count; ++cnt)
+            if (system.contains(dstr(reng))) ++good;
+        return
+            static_cast<float>(good)
+            /static_cast<float>(test_count);
+    }
+};
+
+int main() {
+    DiscreteState d(0);
+    SegmentState s(0, 100);
+    ProbabilityTest pt(-1000, 1000);
+    std::cout << pt.test(d, 20000, 1) << std::endl;
+    std::cout << pt.test(s, 20000, 1) << std::endl;
+    return 0;
+}
