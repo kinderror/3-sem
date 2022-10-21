7dda5394f05a596cb4498075691df71dc17ee9fc
kinderror <dshkinderov@mail.ru>
2022-10-21 20:58:26 +0300

    created queue

--- labTempl.cpp
+++ labTempl.cpp
@@ -43,11 +43,85 @@ void quick_sort(Comparator<T> &compare, T *buffer, int left, int right) {
     quick_sort(compare, buffer, split++, right);
 }
 
+template<typename T>
+class PriorityQueue
+{
+private:
+    T *buffer;
+    Comparator<T> &compare;
+public:
+    PriorityQueue(Comparator<T> &compare): 
+        buffer(new T[0]), compare(compare) {}
+    void push(T value)
+    {
+        int idx = 0, size = sizeof(buffer);
+        T *temp = new T[size + 1];
+        for(; idx < size; idx++)
+        {
+            temp[idx] = buffer[idx];
+        }
+        temp[idx++] = value;
+        while(idx > 0 && 
+            compare(temp[(idx - 1) / 2], temp[idx]))
+        {
+            std::swap(temp[(idx - 1) / 2], temp[idx]);
+            idx = (idx - 1) / 2;
+        }
+        delete [] buffer;
+        buffer = temp;
+    }
+    void heapify()
+    {
+        int idx = 0, left = 1, right = 2,
+            largest = 0, size = sizeof(buffer);
+        
+        while(true)
+        {
+            if(left < size && 
+                compare(buffer[largest], buffer[left]))
+            {
+                largest = left;
+            }
+            if(right < size &&
+                compare(buffer[largest], buffer[right]))
+            {
+                largest = right;
+            }
+            if(largest == idx) return;
+            std::swap(buffer[idx], buffer[largest]);
+            idx = largest;
+        }
+    }
+    void poll()
+    {
+        int size = sizeof(buffer);
+        std::swap(buffer[0], buffer[size - 1]);
+        T *temp = new T[size - 1];
+        for(int idx; idx < size - 1; idx++)
+        {
+            temp[idx] = buffer[idx];
+        }
+        delete [] buffer;
+        buffer = temp;
+        heapify();
+    }
+    void print() const
+    {
+        for(int idx = 0; idx < sizeof(buffer); idx++)
+        {
+            std::cout << buffer[idx] << " ";
+        }
+        std::cout << "\n";
+    }
+    T peek() const { return buffer[0]; }
+    bool is_empty() const { return sizeof(buffer) == 0; }
+    void free() { delete [] buffer; }
+};
+
 int main()
 {
-    std::cout << "sth";
     IntComparator compare = IntComparator();
-    int *buffer = new int[10];
+    /*int *buffer = new int[10];
     for(int i = 0; i < 10; i++)
     {
         buffer[i] = 10 - i;
@@ -57,6 +131,19 @@ int main()
     for(int i = 0; i < 10; i++)
     {
         std::cout << buffer[i] << " ";
+    }*/
+    PriorityQueue<int> queue(compare);
+    if(queue.is_empty()) 
+    {
+        std::cout << "Queue is empty...\n";
     }
+    for(int i = 0; i < 10; i++)
+    {
+        queue.push(i + 1);
+        queue.print();
+    }
+    queue.poll();
+    queue.print();
+    std::cout << queue.peek() << "\n";
     return 0;
 }
